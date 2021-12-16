c78b258c43b6ff6f78d833938066ff5b39b067dd
GD <gd.dev@libertymail.net>
2021-12-16 11:38:36 +0100

    setup prediag python package

---
+++ src/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+# This file is part of the `prediag` package which is released under GPL-v3.
+# See the attached files LICENSE.txt and COPYING.txt for full license details.
+
+from setuptools import setup, find_packages
+
+setup(
+    name='prediag',
+    version='1.0.1',
+    description='Noninvasive Prenatal Diagnosis',
+    author='Ghislain Durif',
+    author_email='gd.dev@libertymail.net',
+    url=None,
+    license='GPL3',
+    license_files = ('LICENSE.txt', 'COPYING.txt'),
+    packages=find_packages(),
+    install_requires=[
+        "joblib",
+        "numpy",
+        "pandas",
+        "psutil",
+        "pyvcf",
+        "scipy",
+        "tqdm"
+    ],
+    entry_points={
+        'console_scripts':
+            ['prediag_fetal_fraction=prediag.cli.fetal_fraction_estimation:main',
+             'prediag_fetal_genotyping=prediag.cli.fetal_genotyping:main',
+             'prediag_fetal_allele_origin_heuristic=prediag.cli.fetal_allele_origin_heuristic:main',
+             'prediag_fetal_allele_origin_bayesian=prediag.cli.fetal_allele_origin_bayesian:main'],
+    },
+    zip_safe=False
+)
